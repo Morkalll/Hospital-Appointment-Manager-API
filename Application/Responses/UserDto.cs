066dfadcca53b4c6a9450e9e87bd04500597d5ac
tboffa <tboffa5@gmail.com>
2026-06-02 12:19:22 -0300

    quickfix exceptionHandlingMiddleware and authController

--- Application/Responses/UserDto.cs
+++ Application/Responses/UserDto.cs
@@ -1,3 +1,8 @@
 namespace TPI_2026.Application.Responses;
 
-public record UserDto(Guid Id, string Name, string Email, string Role);
+public record UserDto(
+    Guid Id,
+    string Name, 
+    string Email, 
+    string Role
+    );
--- Presentation/Controllers/MedicalHistoryController.cs
+++ Presentation/Controllers/MedicalHistoryController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TPI_2026.Domain.Enums;
 using TPI_2026.Application.Abstractions.Interfaces.Services;
+using TPI_2026.Application.Responses;
+using TPI_2026.Application.Requests;
 
 
 
